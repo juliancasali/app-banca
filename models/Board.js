ce705f47432feef31b9cab37725a05f40562db94
juliancasali <juliancasalii@gmail.com>
2025-06-04 23:32:07 -0300

    board

---
+++ models/Board.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const boardSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    student: {
+        type: String,
+        required: true
+    },
+
+    mentor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'teacher'
+    },
+
+    date: {
+        type: Date,
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ["agendada", "concluida"],
+        default: "agendada"
+    },
+
+    course: {
+        type: String
+    },
+
+    tags: {
+        type: [String],
+        default: []
+    },
+},
+    {timestamps: true}
+);
+
+module.exports = mongoose.model('Board', boardSchema);
